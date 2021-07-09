46bad609087fe44851c59a63963601fe317062b3
Josh Courtney <joshrcourtney@gmail.com>
2021-07-09 09:03:10 -0500

    Initial Commit

---
+++ WatchNotes WatchKit Extension/WatchNotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchNotesApp.swift
+//  WatchNotes WatchKit Extension
+//
+//  Created by Josh Courtney on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
